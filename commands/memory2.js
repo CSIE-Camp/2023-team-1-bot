ac96eaa6164e7288ef96fc6c47cb55e4c8400035
liang0929 <111472113+liang0929@users.noreply.github.com>
2023-07-06 16:09:20 +0800

    0706

---
+++ commands/memory2.js
@@ -0,0 +1,43 @@
+const {
+    SlashCommandBuilder,
+    Client,
+    EmbedBuilder,
+    ActionRow,
+    ButtonBuilder,
+    ActionRowBuilder,
+} = require("discord.js");
+const { ButtonStyle } = require("discord.js");
+// discord.js/typings
+module.exports = {
+    data: new SlashCommandBuilder().setName("memory2").setDescription("玩一個記憶卡牌遊戲"),
+    async execute(client, interaction) {
+        let user_name = interaction.user.username;
+        const embed = new EmbedBuilder()
+            .setColor(0x0099ff)
+            .setTitle("來猜牌!")
+            .setDescription("目前玩家:\n" + "你已經翻了: ${interaction.user.username;}")
+            .setTimestamp();
+        interaction.reply({ embeds: [embed] });
+        console.log(user_name);
+    },
+};
+
+// function shuffleArray(array) {
+//     let currentIndex = array.length,
+//         temporaryValue,
+//         randomIndex;
+
+//     // While there remain elements to shuffle...
+//     while (1 !== currentIndex) {
+//         // Pick a remaining element...
+//         randomIndex = Math.floor(Math.random() * currentIndex);
+//         currentIndex -= 1;
+
+//         // And swap it with the current element.
+//         temporaryValue = array[currentIndex];
+//         array[currentIndex] = array[randomIndex];
+//         array[randomIndex] = temporaryValue;
+//     }
+
+//     return array;
+// }
